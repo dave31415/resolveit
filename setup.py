0c0a61c8be42b5f637886f149f0f2298acd530b5
David Johnston <dajohnst@thoughtworks.com>
2021-02-26 21:54:21 -0500

    fixed bug

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='resolveit',
-    version='0.1.0',
+    version='0.1.1',
     description='Resolve data schemas',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
@@ -32,5 +32,4 @@ install_requires = [
 
 if __name__ == '__main__':
     setup(include_package_data=True,
-          **setup_args,
-          install_requires=install_requires)
+          install_requires=install_requires, **setup_args)
